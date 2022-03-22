cd744cba69823a0d23911b1b1cc880c805dcd991
Qarior <101180439+Qarior@users.noreply.github.com>
2022-03-22 10:24:57 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    int a=3;
+    byte b=5;
+    char c=7;
+    double d=155;
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Pierwsza zmienna:"+a);
+    System.out.println("Druga zmienna:"+b);
+    System.out.println("Trzecia zmienna:"+c);
+    System.out.println("Czwarta zmienna:"+d);
+  } 
+}
